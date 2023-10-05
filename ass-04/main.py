0d9918345c775acad138e57ba26dd2cc6122d8c9
choopy <pelayohe94@gmail.com>
2023-10-04 23:26:56 -0600

    Fix minimax algorithm.

--- ass-04/main.py
+++ ass-04/main.py
@@ -1,8 +1,31 @@
 
-from Graph import Node, Graph
+from Graph import Node
+from Board import Board
+
+
+def minimax(root: Node):
+    stack = [root]              # List [Node]
+    while len(stack) > 0:
+        current = stack.pop(0)          # Grab Node
+        board = current.content         # Grab Board
+        while len(board.available) > 0:
+            index = board.available.pop(0)
+            tmp_matrix = board.fill(index)
+            tmp_turn = "o" if board.turn == "x" else "x"
+            tmp_board = Board(matrix=tmp_matrix, turn=tmp_turn)
+            tmp_child = Node(identifier="", content=tmp_board)
+            current.connect(target=tmp_child, weight=0)
+            stack.append(tmp_child)
+
 
 def main():
-    pass
+
+    initial_board = ["o", "x", "o",
+                     "-", "x", "o",
+                     "-", "o", "x"]
+
+    initial_node = Node(identifier="", content=Board(matrix=initial_board, turn="x"))
+    minimax(initial_node)
 
 
 main()
