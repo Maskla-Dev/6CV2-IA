b5bbe833d583b78c60b60a3beaa7447fb6bde5c8
choopy <pelayohe94@gmail.com>
2023-10-04 22:42:51 -0600

    Add Graph class.

---
+++ ass-04/Graph.py
@@ -0,0 +1,75 @@
+
+class Edge:
+
+    def __init__(self, source, target, weight):
+        self.source = source    # Node
+        self.target = target    # Node
+        self.weight = weight    # int
+
+    def __str__(self):
+        return f"{self.source} to {self.target} costs {self.weight}"
+
+
+class Node:
+
+    def __init__(self, identifier, content = None):
+        # ID
+        self.identifier = identifier    # str
+        # Status
+        self.state = {                  # Dict[str, bool]
+            "visited": False
+        }
+        # Content
+        self.content = content
+        # Edge Lookup
+        self.edges = []                 # List[Edge]
+
+    def __str__(self):
+        return f"{self.identifier}"
+
+    def lookup(self, identifier):       # -> Edge or None
+        for edge in self.edges:
+            if edge.target.identifier == identifier:
+                return edge
+        return None
+
+    def connect(self, target, weight):  # target: Node, weight: int
+        if self.lookup(target.identifier) is None:
+            self.edges.append(Edge(self, target, weight))
+
+
+class Graph:
+
+    def __init__(self, name):
+        # ID
+        self.name = name        # str
+        # Node Lookup
+        self.nodes = []         # List[Node]
+
+    def print(self):
+        for node in self.nodes:
+            print(node)
+            for edge in node.edges:
+                print(f"  {edge}")
+
+    def lookup(self, identifier):     # -> Node or None
+        for node in self.nodes:
+            if node.identifier == identifier:
+                return node
+        return None
+
+    def add(self, node):
+        if self.lookup(node.identifier) is None:
+            self.nodes.append(node)
+
+    def connect(self, id1, id2, weight):
+        if self.lookup(id1) is None:
+            raise AttributeError(f"Graph does not contains Node {id1}")
+        if self.lookup(id2) is None:
+            raise AttributeError(f"Graph does not contains Node {id2}")
+        self.lookup(id1).connect(target=self.lookup(id2), weight=weight)
+        self.lookup(id2).connect(target=self.lookup(id1), weight=weight)
+
+    def reset(self):
+        for node in self.nodes:
+            node.state["visited"] = False
