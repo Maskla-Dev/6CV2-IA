0166a7711231799bd210e4915a7412ed55ea67ef
Luis Antonio Montoya Morales <devlumniom@gmail.com>
2023-10-19 18:31:52 -0600

    Update Board.py
    Verificacion de estado

--- ass-04/Board.py
+++ ass-04/Board.py
@@ -21,3 +21,18 @@ class Board:
         print(f"{space}{self.matrix[3]}|{self.matrix[4]}|{self.matrix[5]}")
         print(f"{space}{self.matrix[6]}|{self.matrix[7]}|{self.matrix[8]}")
         print("")
+        
+    def check_winner(self):
+        # Define the winning combinations
+        winning_combinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Columns
+            [0, 4, 8], [2, 4, 6]             # Diagonals
+        ]
+
+        for combination in winning_combinations:
+            a, b, c = combination
+            if self.matrix[a] == self.matrix[b] == self.matrix[c] and self.matrix[a] != "-":
+                return self.matrix[a]  # Return the winning symbol (X or O)
+
+        return None  # No winner yet
