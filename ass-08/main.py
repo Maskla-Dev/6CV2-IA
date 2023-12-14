20d2c1f3246cfe4ccfc5258ece872360b87dc027
choopy <pelayohe94@gmail.com>
2023-12-13 19:57:47 -0600

    Create assessment 08 main file.

---
+++ ass-08/main.py
@@ -0,0 +1,12 @@
+
+# Importamos las bibliotecas que vamos a utilizar.
+
+from sklearn import datasets
+
+
+def main():
+    # Importamos conjunto de datos y almacenamos en variable.
+    dataset = datasets.load_breast_cancer()
+
+
+main()
